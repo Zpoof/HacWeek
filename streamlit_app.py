4fc2e940f7313eefb8b4107af1209cca688a1d27
Zpoof <72591052+Zpoof@users.noreply.github.com>
2021-07-28 10:30:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -672,7 +672,7 @@ if st.button('Find Food!'):
       p = textwrap.fill(p, 90)
       st.text(p)
   else:
-    st.text(textwrap.fill((u[0].get('step')).replace(".", "\n. ")))
+    st.text((u[0].get('step')).replace(".", ".\n- "))
 
 
 
